2fc6db6f62ec2aff33234b78b6ff2d2c351f6266
Jadson-Js <jadson20051965@gmail.com>
2022-08-12 09:04:17 -0300

    Version of SEO, test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+  const { join } = require('path');
+  const PrerenderPlugin = require('prerender-spa-plugin-next');
+
+  plugins.unshift(
+    new PrerenderPlugin({
+      staticDir: join(__dirname, 'dist'),
+      routes: ['/', '/:category', '/:category/noticia/:noticeId/:noticeTitle', '/user/login', '/user/perfil', '/perfil/noticiar', '/perfil/edit/noticia/:noticeId/:noticeTitle', '/perfil/edit/categoria/:categoryId/:categoryTitle', '/:pathMatch(.*)*'], //the page route you want to prerender
+    })
+  );
+}
